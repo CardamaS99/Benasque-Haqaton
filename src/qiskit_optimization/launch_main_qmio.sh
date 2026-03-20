661769682e14cf32ff48ffd68fcc02be3fcc6cf9
marta <mlosada@cesga.es>
2026-03-20 01:26:07 +0100

    committt

--- src/qiskit_optimization/launch_main_qmio.sh
+++ src/qiskit_optimization/launch_main_qmio.sh
@@ -3,6 +3,8 @@
 #SBATCH --partition=qpu
 #SBATCH --output=output.log
 #SBATCH --error=error.log
+#SBATCH --mem=8GB
+#SBATCH --time=01:00:00
 
 conda deactivate
 module load qmio/hpc  gcc/12.3.0  matplotlib jupyter-bundle qmio-run  matplotlib
